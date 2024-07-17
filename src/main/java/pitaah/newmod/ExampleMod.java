6269affd6c77a011bd8302a5b16908d536e5d241
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2024-07-17 14:17:21 +0200

    Initial commit

---
+++ src/main/java/pitaah/newmod/ExampleMod.java
@@ -0,0 +1,37 @@
+package pitaah.newmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.util.GameStartEntrypoint;
+import turniplabs.halplibe.util.RecipeEntrypoint;
+
+
+public class ExampleMod implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+    public static final String MOD_ID = "newmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+        LOGGER.info("ExampleMod initialized.");
+    }
+
+	@Override
+	public void beforeGameStart() {
+
+	}
+
+	@Override
+	public void afterGameStart() {
+
+	}
+
+	@Override
+	public void onRecipesReady() {
+
+	}
+
+	@Override
+	public void initNamespaces() {
+
+	}
+}
